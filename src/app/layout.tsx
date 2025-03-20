a2c719dc2c243a0f33751bd893633f9b5c7d9431
Mahmoud Yousif <myousif123@gmail.com>
2025-03-19 20:15:23 -0400

    feat(site): Added new favicon and less generic metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
+import Head from 'next/head';
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -7,8 +8,8 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Full Stack Developer Portfolio",
-  description: "Personal portfolio website showcasing my work and skills as a full stack developer",
+  title: "Mahmoud Yousif",
+  description: "Personal portfolio website showcasing Mahmoud's work and skills as a full stack developer",
 }
 
 export default function RootLayout({
@@ -18,6 +19,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <link
+          rel="icon"
+          href="/light-logo.ico"
+          media="(prefers-color-scheme: light)"
+        />
+        <link
+          rel="icon"
+          href="/dark-logo.ico"
+          media="(prefers-color-scheme: dark)"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
